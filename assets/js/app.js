3b31e79a58d05ee5a8c88f88a36430d59ebf06fb
Wendy Meneses <wendy.mh.13@gmail.com>
2025-01-23 11:15:19 -0600

    Marqué los errores encontrados

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,25 @@
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
+const $n = document.querySelector('#name');// Le agregué el # para que se acoplara a los datos.
 const $b = document.querySelector('#blog');
 const $l = document.querySelector('.location');
 
-function displayUser(username) {
+async function displayUser(username) {//Le agregué el async para que funcionara el await
   $n.textContent = 'cargando...';
+ try{ // Le agregué el try - catch como me acordé que lo usamos.
   const response = await fetch(`${usersEndpoint}/${username}`);
   console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
-}
+  $n.textContent = `${data.name}`;// A los tres les cambié la comilla sencilla a el acento al revéz porque aparecía solo como texto y no agarraba el ${}
+  $b.textContent = `${data.blog}`;
+  $l.textContent = `${data.location}`;
+} catch (err){
+  handleError(err);
+}}
 
 function handleError(err) {
   console.log('OH NO!');
   console.log(err);
-  n.textContent = `Algo salió mal: ${err}`
+  $n.textContent = `Algo salió mal: ${err}`; // Le cambié de n a $n para que fuera la misma constante
 }
 
-displayUser('stolinski').catch(handleError);
+displayUser('stolinski').catch(handleError(err));
